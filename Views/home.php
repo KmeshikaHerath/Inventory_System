102ab47a6d63e3431374aec03866982e7eec088e
KmeshikaHerath <kameshikajayherath@gmail.com>
2026-04-07 15:08:32 +0530

    Views

---
+++ Views/home.php
@@ -0,0 +1,14 @@
+<?php
+
+        ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title><?= htmlspecialchars($title); ?></title>
+        </head>
+        <body>
+            <h1><?= htmlspecialchars($message); ?></h1>
+            <p>This is a View page.</p>
+        </body>
+        </html>
+    
--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,10 +2,23 @@
 namespace App\Controllers;
 
 use Symfony\Component\HttpFoundation\Response;
+use App\Core\View; 
 
-class HomeController {
+class HomeController
+ {
 
-    public static function home() {
+    public static function home(): Response
+    {
         return new Response("Welcome to the Home Page!");
     }
-}
+
+    public static function notifications(): Response
+    {
+        $html = View::render('home', [
+            'title' => 'Home Page',
+            'message' => 'Welcome to the View Page!'
+        ]);
+
+        return new Response($html);
+    }
+ }
